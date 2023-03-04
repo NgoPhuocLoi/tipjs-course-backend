b2b0e1379232d3ed7e64e95e84fc1e2b4655b56d
NgoPhuocLoi <phuocloi11223@gmail.com>
2023-03-04 10:32:18 +0700

    api sign-up shop, jwt with RSA algorithm

--- src/app.js
+++ src/app.js
@@ -3,26 +3,23 @@ const compression = require("compression");
 const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const { checkOverloadConnection } = require("./helpers/checkConnectDb");
+// const { checkOverloadConnection } = require("./helpers/checkConnectDb");
+const router = require("./routes");
 
 const app = express();
 
 // init middleware
-app.use(morgan("combined"));
+app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // init db
 require("./dbs/mongodb.init");
-checkOverloadConnection();
+// checkOverloadConnection();
 // init routes
-app.get("/", (req, res) => {
-  const str = "helo hello";
-  res.json({
-    message: str.repeat(100000),
-  });
-});
-
+app.use("/", router);
 // handling errors
 
 module.exports = app;
